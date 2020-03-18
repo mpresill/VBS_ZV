c49eb4698a56cf49fc3618faa2c199447ab7dc39
mpresill <26869915+mpresill@users.noreply.github.com>
2020-03-18 17:33:33 +0100

    Update cuts_v3.py

--- 2017/cuts_v3.py
+++ 2017/cuts_v3.py
@@ -1,32 +1,41 @@
 # cuts
-supercut_vector = [#lepton selection: ee (mumu) pt >40(30) |eta|<2.5 (2.4), around Z peak, pt>50 GeV
-  'nLepton == 2' ,
-  '(Lepton_pdgId[0]*Lepton_pdgId[1] == - 11*11 && Alt$(Lepton_pt[0],0.)>=50 && Alt$(Lepton_pt[1],0.)>=50  && fabs(Alt$(Lepton_eta[0],-9999.))<2.5 && fabs(Alt$(Lepton_eta[1],-9999.))<2.5) || (Lepton_pdgId[0]*Lepton_pdgId[1] == - 13*13 && Alt$(Lepton_pt[0],0.)>50 && Alt$(Lepton_pt[1],0.)>50  && fabs(Alt$(Lepton_eta[0],-9999.))<2.4 && fabs(Alt$(Lepton_eta[1],-9999.))<2.4)', 
+# NB: mjj and detajj cannot be used!!!! 
+supercut_vector = [#2 lepton selection:  pt >40(30) |eta|<2.5 (2.4) pt>50 GeV
+  'nLepton == 2 && && Alt$(Lepton_pt[0],0.)>=50 && Alt$(Lepton_pt[1],0.)>=30',
 
-#jet selection: 1 FJ, pt>200 GeV, |eta|<2.4  + 2 jets with pt>30 and |eta|<2.4 
+#jet selection: 1 FJ, pt>200 GeV, |eta|<2.4  + 2 jets with pt>30 and |eta|<5 and with mjj>200GeV and detajj>2.0
 'nFatJet == 1 && FatJet_pt[0] >= 200. && fabs(Alt$(FatJet_eta[0],-9999.))<2.4',
-'Sum$(CleanJet_pt>30.)>=2 '
+'Sum$(CleanJet_pt>30.)>=2 && fabs(Alt$(CleanedJet_eta[0],-9999.))<5.0',
+ 'mjj > 200 && detajj > 2.0'
 ]
-
+#at some point we should add a selectio of FatJet in case of more than one candidate
 
 supercut = ' && '.join(supercut_vector)
 
+#####Signal Regions
+cuts['Mild_SR'] ='(Lepton_pdgId[0]*Lepton_pdgId[1] == - 11*11 && Alt$(Lepton_pt[0],0.)>=50 && Alt$(Lepton_pt[1],0.)>=50  && fabs(Alt$(Lepton_eta[0],-9999.))<2.5 && fabs(Alt$(Lepton_eta[1],-9999.))<2.5) || (Lepton_pdgId[0]*Lepton_pdgId[1] == - 13*13 && Alt$(Lepton_pt[0],0.)>50 && Alt$(Lepton_pt[1],0.)>50  && fabs(Alt$(Lepton_eta[0],-9999.))<2.4 && fabs(Alt$(Lepton_eta[1],-9999.))<2.4) \ 
+             && Alt$(FatJet_msoftdrop[0],0.)>65 && Alt$(FatJet_msoftdrop[0],0.)<105 && Alt$(FatJet_tau2[0]/FatJet_tau1[0],0.)<0.55\
+             && mll>76 && mll<107 \
+             && Sum$(Jet_btagDeepB[CleanJet_jetIdx] > 0.1522) == 0 '
 
-cuts['SR'] ='1.'
+cuts['Tight_SR'] = '(Lepton_pdgId[0]*Lepton_pdgId[1] == - 11*11 && Alt$(Lepton_pt[0],0.)>=50 && Alt$(Lepton_pt[1],0.)>=50  && fabs(Alt$(Lepton_eta[0],-9999.))<2.5 && fabs(Alt$(Lepton_eta[1],-9999.))<2.5) || (Lepton_pdgId[0]*Lepton_pdgId[1] == - 13*13 && Alt$(Lepton_pt[0],0.)>50 && Alt$(Lepton_pt[1],0.)>50  && fabs(Alt$(Lepton_eta[0],-9999.))<2.4 && fabs(Alt$(Lepton_eta[1],-9999.))<2.4) \ 
+             && Alt$(FatJet_msoftdrop[0],0.)>65 && Alt$(FatJet_msoftdrop[0],0.)<105 && Alt$(FatJet_tau2[0]/FatJet_tau1[0],0.)<0.55\
+             && mll>76 && mll<107 \
+             && Sum$(Jet_btagDeepB[CleanJet_jetIdx] > 0.1522) == 0 \
+             && mjj>800 && detajj>4.0'
+
+#########Control regions
+
+cuts['DY_CR'] = '(Lepton_pdgId[0]*Lepton_pdgId[1] == - 11*11 && Alt$(Lepton_pt[0],0.)>=50 && Alt$(Lepton_pt[1],0.)>=50  && fabs(Alt$(Lepton_eta[0],-9999.))<2.5 && fabs(Alt$(Lepton_eta[1],-9999.))<2.5) || (Lepton_pdgId[0]*Lepton_pdgId[1] == - 13*13 && Alt$(Lepton_pt[0],0.)>50 && Alt$(Lepton_pt[1],0.)>50  && fabs(Alt$(Lepton_eta[0],-9999.))<2.4 && fabs(Alt$(Lepton_eta[1],-9999.))<2.4) \ 
+                && mll>100 && mll<300'
+
+cuts['Top_CR'] = 'Alt$(FatJet_msoftdrop[0],0.)>65 && Alt$(FatJet_msoftdrop[0],0.)<105 && Sum$(Jet_btagDeepB[CleanJet_jetIdx] > 0.1522) >= 1 && ((Lepton_pdgId[0]*Lepton_pdgId[1] == - 11*13 || (Lepton_pdgId[0]*Lepton_pdgId[1] == 11*13)' #SR cuts and bveto condition inverted + 2opposite flavour leptons ---CHECK THIS CR!!
 
 
-cuts['Mild_SR'] = 'Alt$(FatJet_msoftdrop[0],0.)>65 && Alt$(FatJet_msoftdrop[0],0.)<105 && mjj > 200 && detajj > 2.0 && Sum$(Jet_btagDeepB[CleanJet_jetIdx] > 0.1522) == 0 && mll>76 && mll<107'
+cuts['Wjets_CR'] = '(Alt$(FatJet_msoftdrop[0],0.)>40 && Alt$(FatJet_msoftdrop[0],0.)<65) || (Alt$(FatJet_msoftdrop[0],0.)>105 && Alt$(FatJet_msoftdrop[0],0.)<150)  && Sum$(Jet_btagDeepB[CleanJet_jetIdx] > 0.1522) == 0'#SR cuts and msoftdrop window inverted
 
-
-cuts['Tight_SR'] = 'Alt$(FatJet_tau2[0]/FatJet_tau1[0],0.)<0.55 && Alt$(FatJet_msoftdrop[0],0.)>65 && Alt$(FatJet_msoftdrop[0],0.)<105 && mjj > 800 && detajj > 4.0 && Sum$(Jet_btagDeepB[CleanJet_jetIdx] > 0.1522) == 0 && mll>76 && mll<107'
- 
 
 """
-cuts['Top_CR'] = 'Alt$(FatJet_msoftdrop[0],0.)>65 && Alt$(FatJet_msoftdrop[0],0.)<105 && mjj > 800 && detajj > 4.0 && Sum$(Jet_btagDeepB[CleanJet_jetIdx] > 0.1522) >= 1 && mll>76 && mll<107 ' #SR cuts and bveto condition inverted
-
-
-cuts['Wjets_CR'] = '(Alt$(FatJet_msoftdrop[0],0.)>40 && Alt$(FatJet_msoftdrop[0],0.)<65) || (Alt$(FatJet_msoftdrop[0],0.)>105 && Alt$(FatJet_msoftdrop[0],0.)<150) && mjj > 800 && detajj > 4.0 && Sum$(Jet_btagDeepB[CleanJet_jetIdx] > 0.1522) == 0'#SR cuts and msoftdrop window inverted
-
 
 cuts['Boosted']  = 'nFatJet == 1 && FatJet_pt[0] >=200. \
     && nJet >=2 && Jet_pt[0] >= 30. && Jet_pt[1]> 30.'
