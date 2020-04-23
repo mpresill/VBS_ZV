b2a96bf2b350f56f60143da6b89463b0c992a7a8
Matteo Presilla <matteo.presilla@cern.ch>
2020-04-23 18:40:16 +0200

    s

--- 2017_v6/aliases.py
+++ 2017_v6/aliases.py
@@ -101,8 +101,7 @@ aliases['WH2l_pTW'] = {
         'gSystem->Load("libLatinoAnalysisMultiDraw.so")',
         '.L /afs/cern.ch/work/m/mpresill/Latino_workdir/CMSSW_10_6_4/src/PlotsConfigurations/Configurations/VBS_ZV:/macros/whss_wlep_v3.cc+'.format(configurations)
     ],
-    'class': 'WHSS_wpt_v3',
-    'args': ()
+    'class': 'WHSS_wpt_v3'
 }
 
 
