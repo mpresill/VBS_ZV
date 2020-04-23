249481d0acb8c15ee81300e8a62e759b151b93fc
Matteo Presilla <matteo.presilla@cern.ch>
2020-04-23 15:43:47 +0200

    a

--- 2017_v6/aliases.py
+++ 2017_v6/aliases.py
@@ -117,7 +117,7 @@ aliases['detajj_vbs_AK4NotFat'] = {
 aliases['WH2l_pTW'] = {
     'linesToAdd': [
         'gSystem->Load("libLatinoAnalysisMultiDraw.so")',
-        '.L /afs/cern.ch/work/m/mpresill/Latino_workdir/CMSSW_10_6_4/src/PlotsConfigurations/Configurations/VBS_ZV/macros/whss_wlep_v3.cc+'.format(configurations)
+        '.L /afs/cern.ch/work/m/mpresill/Latino_workdir/CMSSW_10_6_4/src/PlotsConfigurations/Configurations/VBS_ZV/macros/wh3l_wlep.cc+'.format(configurations)
     ],
     'class': 'WHSS_wpt_v3',
     'args': ()
