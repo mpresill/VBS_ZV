37caeceaf53bff51264f8cf727ef1b463fc682f2
Matteo Presilla <matteo.presilla@cern.ch>
2020-04-28 18:11:43 +0200

    a

--- 2017_v6/plot.py
+++ 2017_v6/plot.py
@@ -291,7 +291,7 @@ plot['DATA']  = {
                   'color': 1 ,
                   'isSignal' : 0,
                   'isData'   : 1 ,
-                  'isBlind'  : 1 ,
+                  'isBlind'  : 0 ,
                   'scale'    : 1.0
               }
 # additional options
