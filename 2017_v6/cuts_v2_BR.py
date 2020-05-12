af713e0a0894a29bcecbb37e6730c32b0f39afa2
Matteo Presilla <matteo.presilla@cern.ch>
2020-05-12 12:42:03 +0200

    j

--- 2017_v6/cuts_v2_BR.py
+++ 2017_v6/cuts_v2_BR.py
@@ -1,6 +1,6 @@
 supercut_vector = [##2 lepton selection:  pt >30 |eta|<2.5 (2.4) pt>30 GeV
   'nLepton == 2. && Alt$(Lepton_pt[0],0.)>=25. && Alt$(Lepton_pt[1],0.)>=20.',
-  'mll >60. && mll <120.'
+  'mll >60. && mll <120.',
 ##jet selection: 1 FJ, pt>200 GeV, |eta|<2.4  + 2 jets with pt>30 and |eta|<5 and with mjj_vbs>200GeV and detajj_vbs>2.0
  #'nCleanFatJet == 1 && CleanFatJet_pt[0] >= 200. && fabs(Alt$(CleanFatJet_eta[0],-9999.))<2.4',
  'nCleanJetNotFat >= 2 && fabs(Alt$(CleanJet_pt,-9999.))>30. && fabs(Alt$(CleanJet_eta,-9999.))<5.0',  ##'Sum$(CleanJet_pt>30.)>=2 && fabs(Alt$(CleanJet_eta[0],-9999.))<5.0',
