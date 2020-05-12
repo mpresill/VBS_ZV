a7840829b2c7bac328bb4384f84840f5b450e41e
Matteo Presilla <matteo.presilla@cern.ch>
2020-05-12 13:39:32 +0200

    a

--- 2017_v6/cuts_v2.py
+++ 2017_v6/cuts_v2.py
@@ -4,7 +4,7 @@ supercut_vector = [##2 lepton selection:  pt >30 |eta|<2.5 (2.4) pt>30 GeV
 ##jet selection: 1 FJ, pt>200 GeV, |eta|<2.4  + 2 jets with pt>30 and |eta|<5 and with mjj_vbs>200GeV and detajj_vbs>2.0
  'nCleanFatJet == 1 && CleanFatJet_pt[0] >= 200. && fabs(Alt$(CleanFatJet_eta[0],-9999.))<2.4',
  'nCleanJetNotFat >= 2 && fabs(Alt$(CleanJet_pt,-9999.))>30. && fabs(Alt$(CleanJet_eta,-9999.))<5.0',  ##'Sum$(CleanJet_pt>30.)>=2 && fabs(Alt$(CleanJet_eta[0],-9999.))<5.0',
- 'detajj_vbs_AK4NotFat>2.0'
+ 'mjj_vbs_AK4NotFat > 200 && detajj_vbs_AK4NotFat>2.0'
  #'mjj_vbs_AK4NotFat > 200 && eta1eta2 < 0.' 
 ]
 #at some point we should add a selection of FatJet in case of more than one candidate...
@@ -44,11 +44,11 @@ cuts['SR_tight'] = '((Lepton_pdgId[0]*Lepton_pdgId[1] == - 11*11 && Alt$(Lepton_
 ###############################
 
 
-cuts['DYJets_CR'] = 'mjj_vbs_AK4NotFat > 200 && ((Lepton_pdgId[0]*Lepton_pdgId[1] == - 11*11 && Alt$(Lepton_pt[0],0.)>=25. && Alt$(Lepton_pt[1],0.)>=25.  && fabs(Alt$(Lepton_eta[0],-9999.))<2.5 && fabs(Alt$(Lepton_eta[1],-9999.))<2.5)  || (Lepton_pdgId[0]*Lepton_pdgId[1] == - 13*13 && Alt$(Lepton_pt[0],0.)>=20. && Alt$(Lepton_pt[1],0.)>=20.  && fabs(Alt$(Lepton_eta[0],-9999.))<2.4 && fabs(Alt$(Lepton_eta[1],-9999.))<2.4))  && mll>60. && mll<120.\
+cuts['DYJets_CR'] = '((Lepton_pdgId[0]*Lepton_pdgId[1] == - 11*11 && Alt$(Lepton_pt[0],0.)>=25. && Alt$(Lepton_pt[1],0.)>=25.  && fabs(Alt$(Lepton_eta[0],-9999.))<2.5 && fabs(Alt$(Lepton_eta[1],-9999.))<2.5)  || (Lepton_pdgId[0]*Lepton_pdgId[1] == - 13*13 && Alt$(Lepton_pt[0],0.)>=20. && Alt$(Lepton_pt[1],0.)>=20.  && fabs(Alt$(Lepton_eta[0],-9999.))<2.4 && fabs(Alt$(Lepton_eta[1],-9999.))<2.4))  && mll>60. && mll<120.\
     && bVeto'#&& detajj_vbs_AK4NotFat >3.5
 
 
-cuts['Top_CR'] = 'mjj_vbs_AK4NotFat > 200 && bReq && ((Lepton_pdgId[0]*Lepton_pdgId[1] == - 11*13) || (Lepton_pdgId[0]*Lepton_pdgId[1] == 11*13))'
+cuts['Top_CR'] = 'bReq && ((Lepton_pdgId[0]*Lepton_pdgId[1] == - 11*13) || (Lepton_pdgId[0]*Lepton_pdgId[1] == 11*13))'
 
 
 """
