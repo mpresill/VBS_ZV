ff16300c76f6ae64c2a1fab0123a9c860de6156b
Matteo Presilla <matteo.presilla@cern.ch>
2020-05-12 12:39:58 +0200

    h

--- 2017_v6/cuts_v2_BR.py
+++ 2017_v6/cuts_v2_BR.py
@@ -18,25 +18,22 @@ cuts['preselection'] = '1.'
 #############################
 ######### BOOSTED SR
 #############################
-cuts['SR_boosted_minimal']= '2lSF &&\
-                    mll >80. && mll <100. &&\
+cuts['SR_boosted_minimal']= '((Lepton_pdgId[0]*Lepton_pdgId[1] == - 11*11 && Alt$(Lepton_pt[0],0.)>=25. && Alt$(Lepton_pt[1],0.)>=25.  && fabs(Alt$(Lepton_eta[0],-9999.))<2.5 && fabs(Alt$(Lepton_eta[1],-9999.))<2.5)  || (Lepton_pdgId[0]*Lepton_pdgId[1] == - 13*13 && Alt$(Lepton_pt[0],0.)>=25. && Alt$(Lepton_pt[1],0.)>=20.  && fabs(Alt$(Lepton_eta[0],-9999.))<2.4 && fabs(Alt$(Lepton_eta[1],-9999.))<2.4 ) ) &&  mll >80. && mll <100. &&\
                     Alt$(CleanJet_pt[0],0.)>=80. && Alt$(CleanJet_pt[1],0.)>=50.'
 
-cuts['SR_boosted_leptons']= '2lSF &&\
-                    mll >80. && mll <100. &&\
+cuts['SR_boosted_leptons']= '((Lepton_pdgId[0]*Lepton_pdgId[1] == - 11*11 && Alt$(Lepton_pt[0],0.)>=25. && Alt$(Lepton_pt[1],0.)>=25.  && fabs(Alt$(Lepton_eta[0],-9999.))<2.5 && fabs(Alt$(Lepton_eta[1],-9999.))<2.5)  || (Lepton_pdgId[0]*Lepton_pdgId[1] == - 13*13 && Alt$(Lepton_pt[0],0.)>=25. && Alt$(Lepton_pt[1],0.)>=20.  && fabs(Alt$(Lepton_eta[0],-9999.))<2.4 && fabs(Alt$(Lepton_eta[1],-9999.))<2.4 ) ) &&   mll >80. && mll <100. &&\
                     Alt$(Lepton_pt[0],0.)>=40. && Zlep_1 < 1.0 && Zlep_2 < 1.0  && Alt$(CleanJet_pt[0],0.)>=80. && Alt$(CleanJet_pt[1],0.)>=50. '
 
-cuts['SR_boosted_j1j2']= '2lSF &&\
-                    mll >80. && mll <100. &&\
+cuts['SR_boosted_j1j2']= '((Lepton_pdgId[0]*Lepton_pdgId[1] == - 11*11 && Alt$(Lepton_pt[0],0.)>=25. && Alt$(Lepton_pt[1],0.)>=25.  && fabs(Alt$(Lepton_eta[0],-9999.))<2.5 && fabs(Alt$(Lepton_eta[1],-9999.))<2.5)  || (Lepton_pdgId[0]*Lepton_pdgId[1] == - 13*13 && Alt$(Lepton_pt[0],0.)>=25. && Alt$(Lepton_pt[1],0.)>=20.  && fabs(Alt$(Lepton_eta[0],-9999.))<2.4 && fabs(Alt$(Lepton_eta[1],-9999.))<2.4 ) ) &&  mll >80. && mll <100. &&\
                     Alt$(Lepton_pt[0],0.)>=40.  && Alt$(CleanJet_pt[0],0.)>=80. && Alt$(CleanJet_pt[1],0.)>=50. &&  mjj_vbs_AK4NotFat >  300.'
 
 
 #############################
 ######### BOOSTED CR
 #############################
-cuts['DYJets_CR_boosted'] = '2lSF && bVeto '
+cuts['DYJets_CR_boosted'] = '((Lepton_pdgId[0]*Lepton_pdgId[1] == - 11*11 && Alt$(Lepton_pt[0],0.)>=25. && Alt$(Lepton_pt[1],0.)>=25.  && fabs(Alt$(Lepton_eta[0],-9999.))<2.5 && fabs(Alt$(Lepton_eta[1],-9999.))<2.5)  || (Lepton_pdgId[0]*Lepton_pdgId[1] == - 13*13 && Alt$(Lepton_pt[0],0.)>=25. && Alt$(Lepton_pt[1],0.)>=20.  && fabs(Alt$(Lepton_eta[0],-9999.))<2.4 && fabs(Alt$(Lepton_eta[1],-9999.))<2.4 ) ) && bVeto '
 
-cuts['Top_CR_boosted'] = 'bReq && 2lOF'
+cuts['Top_CR_boosted'] = '((Lepton_pdgId[0]*Lepton_pdgId[1] == - 11*13 && Alt$(Lepton_pt[0],0.)>=25. && Alt$(Lepton_pt[1],0.)>=25.  && fabs(Alt$(Lepton_eta[0],-9999.))<2.5 && fabs(Alt$(Lepton_eta[1],-9999.))<2.5)  || (Lepton_pdgId[0]*Lepton_pdgId[1] == 11*13 && Alt$(Lepton_pt[0],0.)>=25. && Alt$(Lepton_pt[1],0.)>=20.  && fabs(Alt$(Lepton_eta[0],-9999.))<2.4 && fabs(Alt$(Lepton_eta[1],-9999.))<2.4 ) ) && bReq'
 
 #############################
 ######### RESOLVED SR
