caf7e6cc5f85bde98beed98c74151cf34cdbd449
Matteo Presilla <matteo.presilla@cern.ch>
2020-05-14 18:14:31 +0200

    ad

--- 2017_v6/cuts_HCMN_check.py
+++ 2017_v6/cuts_HCMN_check.py
@@ -1,9 +1,7 @@
 supercut = '   mll>300 \
             && Lepton_pt[0]>150 \
             && Lepton_pt[1]>100 \
-            && (Lepton_pdgId[0]*Lepton_pdgId[0] == - 11*11)  && (Lepton_pdgId[1]*Lepton_pdgId[1] == - 13*13) \
-            && nCleanFatJet>=1\
-           '
+            && nCleanFatJet >= 1 '
 
 cuts['preselection'] = '1.'
 
@@ -11,4 +9,4 @@ cuts['preselection'] = '1.'
 #############################
 ######### TRIGGER TEST
 #############################
-cuts['SR_plus_trigger']= 'HLT_Photon200==1 || HLT_Ele35_WPTight_Gsf==1'
+cuts['SR_plus_trigger']= '(Lepton_pdgId[0]*Lepton_pdgId[0] == - 11*11)  && (Lepton_pdgId[1]*Lepton_pdgId[1] == - 13*13) && (HLT_Photon200==1 || HLT_Ele35_WPTight_Gsf==1)'
