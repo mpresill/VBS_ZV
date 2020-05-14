68b44be6a1315949742f6a6fa4e374b0c91ee5a0
Matteo Presilla <matteo.presilla@cern.ch>
2020-05-14 13:09:45 +0200

    a

--- 2017_v6/variables_HCMN.py
+++ 2017_v6/variables_HCMN.py
@@ -3,7 +3,7 @@
 
 #variables = {}
 
- variables['events']  = {   'name': '1',
+svariables['events']  = {   'name': '1',
                            'range' : (1,0,2),
                            'xaxis' : 'events',
                            'fold' : 3
