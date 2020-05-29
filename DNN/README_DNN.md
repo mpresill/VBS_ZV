35472b78d3cceae8b7633cb391ad3bb07f3c188c
Matteo Presilla <matteo.presilla@cern.ch>
2020-05-29 15:59:22 +0200

    dnn

---
+++ DNN/README_DNN.md
@@ -0,0 +1,6 @@
+To import the sample list in the correct json format use:
+
+python unfold_sample.py\
+  -i .samples.py \
+  -o samples_v1.json \
+  -s Wjets DY top VZ VVV VBS_VV_QCD VBS_VV_EW
---
+++ DNN/variables.py
@@ -0,0 +1,39 @@
+# variabeadines
+
+#variables = {}
+    
+
+variables['events']  = {   'name': '1',      
+                        'range' : (1,0,2),  
+                        'xaxis' : 'events', 
+                        'fold' : 3
+                        }
+
+
+
+#leptons
+
+
+variables['Lepton_eta1'] = {   'name': 'Lepton_eta[0]',      
+                        'range' : (30,-3,3),  
+                        'xaxis' : 'Lepton #eta', 
+                        'fold' : 3
+                        }
+
+variables['Lepton_pt1'] = {   'name': 'Lepton_pt[0]',      
+                        'range' : (30,0,500),  
+                        'xaxis' : 'Lepton pt', 
+                        'fold' : 3
+                        }                       
+
+variables['Lepton_eta2'] = {   'name': 'Lepton_eta[1]',      
+                        'range' : (30,-3,3),  
+                        'xaxis' : 'Lepton #eta', 
+                        'fold' : 3
+                        }
+
+variables['Lepton_pt2'] = {   'name': 'Lepton_pt[1]',      
+                        'range' : (30,0,500),  
+                        'xaxis' : 'Lepton pt', 
+                        'fold' : 3
+                        }                       
