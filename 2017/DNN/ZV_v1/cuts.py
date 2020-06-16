2d129570cb78af80ed01de328d605169edbcd8e7
Alexandre Hakimi <ahakimi@lxplus7122.cern.ch>
2020-06-16 19:19:35 +0200

    updated config files

--- 2017/DNN/ZV_v1/cuts.py
+++ 2017/DNN/ZV_v1/cuts.py
@@ -1,16 +1,35 @@
 cuts["supercut"] = {
     'expr' : ' nLepton == 2. \
+            && Lepton_pt[0]>25 \
+            && Lepton_pt[1]>20  \
+            && nCleanJetNotFat >= 2 \
+            &&  abs(Lepton_eta[0])<2.5 && abs(Lepton_eta[1])<2.5 \
+            && Alt(Take(CleanJet_pt, CleanJetNotFat_jetIdx),0,-9999.)>30. \
+            && abs(Alt(Take(CleanJet_eta,CleanJetNotFat_jetIdx),0,-9999.))<5.0 \
             && mll >60. && mll <120.',
+            
     'parent' : None,
     'doVars' : False,
     'doNumpy': False
 }
+#no mjj/detajj cut since we want to have a look at their distrib with BDT
+
+#cuts['preselection'] = '1.'
 
 
+##a selection on VBS_jets is still required
 
+#############################
+######### BOOSTED SR
+#############################
 cuts['SR_boosted_minimal'] = {
-    'expr': 'nCleanFatJet == 1 && CleanFatJet_pt[0] >= 200.',
+    'expr': '((Lepton_pdgId[0]*Lepton_pdgId[1] == - 11*11)  || (Lepton_pdgId[0]*Lepton_pdgId[1] == - 13*13 )) \
+        && nCleanFatJet == 1 && CleanFatJet_pt[0] >= 200. \
+        && abs(Alt(CleanFatJet_eta,0,-9999.))<2.4',
+     
     'parent' : 'supercut',
     'doVars' : True,
     'doNumpy': True
     }
+
+
--- 2017/DNN/ZV_v1/variables.py
+++ 2017/DNN/ZV_v1/variables.py
@@ -3,7 +3,7 @@
 #variables = {}
 
 
-variables['mllpeak'] = {   'name': 'mll',            #   variable name
+variables['mll'] = {   'name': 'mll',            #   variable name
                            'range' : (80,80,100),    #   variable range
                            'xaxis' : 'm_{ll} [GeV]',  #   x axis name
                            'fold' : 0
@@ -45,11 +45,11 @@ variables['eta2']  = {  'name': 'Lepton_eta[1]',
 # jets
 #
 
-#variables['mjj'] = {   'name': 'mjj_vbs',            #   variable name
- #                          'range' : (50,0,1500),    #   variable range
-  #                         'xaxis' : 'VBS jets mass [GeV]',  #   x axis name
-   #                        'fold' : 0
-    #                    }
+variables['mjj'] = {   'name': 'mjj',            #   variable name
+                           'range' : (50,0,1500),    #   variable range
+                           'xaxis' : 'VBS jets mass [GeV]',  #   x axis name
+                           'fold' : 0
+                        }
 
 #variables['mjj_NotFat'] = {   'name': 'mjj_vbs_AK4NotFat',            #   variable name
  #                          'range' : (50,0,1500),    #   variable range
@@ -58,16 +58,16 @@ variables['eta2']  = {  'name': 'Lepton_eta[1]',
     #                    }
  
 
-#variables['jetpt1'] = { 'name': 'Alt(CleanJet_pt[0],-9999.)',
-#                        'range': (30,0.,500),
-#                        'xaxis': 'p_{T} 1st jet',
-#                        'fold':0
-#}
-#variables['jetpt2'] = { 'name': 'Alt(CleanJet_pt[1],-9999.)',
-#                        'range': (30,0.,300),
-#                        'xaxis': 'p_{T} 2nd jet',
-#                        'fold':0
-#}
+variables['jetpt1'] = { 'name': 'Alt(CleanJet_pt,0,-9999.)',
+                        'range': (30,0.,500),
+                        'xaxis': 'p_{T} 1st jet',
+                        'fold':0
+}
+variables['jetpt2'] = { 'name': 'Alt(CleanJet_pt,1,-9999.)',
+                        'range': (30,0.,300),
+                        'xaxis': 'p_{T} 2nd jet',
+                        'fold':0
+}
 
 variables['eta_jet1']  = {  'name': 'CleanJet_eta[0]',     
                         'range' : (40,-5,5),   
@@ -96,10 +96,10 @@ variables['nCleanJetNotFat']  = {
 
 
 #variables['btag_cleanjet'] = {   'name': 'Take(Jet_btagDeepB, CleanJet_jetIdx)',            #   variable name
-#                           'range' : (80,0.,1.),    #   variable range
- #                          'xaxis' : 'btag of cleaned jets',  #   x axis name
-  #                         'fold' : 0
-   #                     }
+ #                          'range' : (80,0.,1.),    #   variable range
+  #                         'xaxis' : 'btag of cleaned jets',  #   x axis name
+   #                        'fold' : 0
+    #                    }
 
 
 
@@ -123,20 +123,11 @@ variables['FatJet_mass'] = {   'name': 'CleanFatJet_mass[0]',
 			       'fold': 3
 			       }
 
-#variables['nCleanJet']  = {
- #                       'name': 'SumVec(CleanJet_pt>30)',     
-  #                      'range' : (8,0,8),   
-   #                     'xaxis' : 'Number of jets w/ p_{T}>30 GeV',
-    #                    'fold' : 2   # 0 = not fold (default), 1 = fold underflowbin, 2 = fold overflow bin, 3 = fold underflow and overflow
-     #                   }
-
-
-
-#variables['detajj']  = {  'name': 'detajj_vbs',
- #                         'range': (32,0.0,8.0),
-  #                        'xaxis': '\Delta \eta (jj)',
-   #                       'fold': 3
-    #                      }
+variables['detajj']  = {  'name': 'detajj',
+                          'range': (32,0.0,8.0),
+                         'xaxis': '\Delta \eta (jj)',
+                          'fold': 3
+                          }
 
 
 #variables['detajj']  = {  'name': 'detajj_vbs_AK4NotFat',
@@ -162,4 +153,6 @@ variables['puppimet']  = {
   #                           'xaxis': 'M_{ZV} [GeV]',
    #                          'fold': 3,
     #                        }#function implemented in aliases.py and in M_leplepBjets_class.cc
+
+
 
