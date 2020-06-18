bdbd853de6bc76e51b8487b78731d186cacd84d4
mpresill <26869915+mpresill@users.noreply.github.com>
2020-06-18 11:35:48 +0200

    Update cuts.py

--- 2018_v6/cuts.py
+++ 2018_v6/cuts.py
@@ -7,7 +7,7 @@ supercut = '   nLepton == 2 \
             && Lepton_pt[1]>20 \
             && mll >60. && mll <120. \
             && abs(Lepton_eta[0])<2.5 && abs(Lepton_eta[1])<2.5 \
-            && nCleanJetNotFat >= 2 && fabs(Alt$(CleanJet_pt[CleanJetNotFat_jetIdx],-9999.))>30. && fabs(Alt$(CleanJet_eta[CleanJetNotFat_jetIdx],-9999.))<5.0 \
+            && nCleanJetNotFat >= 2 && fabs(Alt$(CleanJet_pt[CleanJetNotFat_jetIdx],-9999.))>50. && fabs(Alt$(CleanJet_eta[CleanJetNotFat_jetIdx],-9999.))<5.0 \
            '
 #I have considered all CleanedJetNotFat, i.e. cleaned from AK8, since in the case in which nCleanFatJet==0 => nCleanJetNotFat = NCleanJet
 
